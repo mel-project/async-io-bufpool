bc180b77f8064ae1cd2115c13c0905082afbf4c5
nullchinchilla <nullchinchilla@pm.me>
2024-10-24 21:41:30 -0400

    Return buffer to pool on Poll::Pending in PooledOnceReader.poll method

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,10 @@ impl<T: AsyncRead> Future for PooledOnceReader<T> {
                 POOL.push(free_buf);
                 std::task::Poll::Ready(Err(err))
             }
-            std::task::Poll::Pending => std::task::Poll::Pending,
+            std::task::Poll::Pending => {
+                POOL.push(free_buf);
+                std::task::Poll::Pending
+            }
         }
     }
 }
